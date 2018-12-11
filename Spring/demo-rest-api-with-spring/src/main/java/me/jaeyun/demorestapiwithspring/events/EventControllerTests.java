1c8366d0db3974f0ccd841718fdc66c001332c5c
JaeYoon Song <acornjello14@gmail.com>
2018-12-11 16:56:43 +0900

    rest-api : Fail on unknown properties

--- Spring/demo-rest-api-with-spring/src/main/java/me/jaeyun/demorestapiwithspring/events/EventControllerTests.java
+++ Spring/demo-rest-api-with-spring/src/main/java/me/jaeyun/demorestapiwithspring/events/EventControllerTests.java
@@ -32,6 +32,36 @@ public class EventControllerTests {
 
     @Test
     public void createEvent() throws Exception {
+        EventDto event = EventDto.builder()
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2018, 11, 23, 14, 21))
+                .closeEnrollmentDateTime(LocalDateTime.of(2018, 11, 24, 14, 21))
+                .beginEventDateTime(LocalDateTime.of(2018, 11, 25, 14, 21))
+                .endEventDateTime(LocalDateTime.of(2018, 11, 26, 14, 21))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타텁 팩토리") // 값이 있는 경우에는 offline이 true;
+                .build();
+
+        mockMvc.perform(post("/api/events/")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaTypes.HAL_JSON)
+                .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists())
+                .andExpect(header().exists(HttpHeaders.LOCATION))   // "Location" 보다 type-safe
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
+                .andExpect(jsonPath("id").value(CoreMatchers.not(100)))
+                .andExpect(jsonPath("free").value(CoreMatchers.not(true)))
+                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()))
+        ;
+    }
+
+    @Test
+    public void createEvent_Bad_Request() throws Exception {
         Event event = Event.builder()
                 .id(100)
                 .name("Spring")
@@ -43,29 +73,18 @@ public class EventControllerTests {
                 .basePrice(100)
                 .maxPrice(200)
                 .limitOfEnrollment(100)
-                .location("강남역 D2 스타텁 팩토리") // 값이 있는 경우에는 offline이 true;
+                .location("강남역 D2 스타텁 팩토리")
                 .free(true)
                 .offline(false)
                 .eventStatus(EventStatus.PUBLISHED)
                 .build();
 
-        // event.setId(10);
-        // Mockito.when(eventRepository.save(event)).thenReturn(event);
-        // newEvent != event이기 때문에 mocking이 적용이 안되어 null이발생할 수 있음. -> mocking안하기
-        // --> 실제 repository사용됨
-
         mockMvc.perform(post("/api/events/")
-                    .contentType(MediaType.APPLICATION_JSON_UTF8)
-                    .accept(MediaTypes.HAL_JSON)
-                    .content(objectMapper.writeValueAsString(event)))
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaTypes.HAL_JSON)
+                .content(objectMapper.writeValueAsString(event)))
                 .andDo(print())
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("id").exists())
-                .andExpect(header().exists(HttpHeaders.LOCATION))   // "Location" 보다 type-safe
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
-                .andExpect(jsonPath("id").value(CoreMatchers.not(100)))
-                .andExpect(jsonPath("free").value(CoreMatchers.not(true)))
-                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()))
+                .andExpect(status().isBadRequest())
         ;
 
     }
