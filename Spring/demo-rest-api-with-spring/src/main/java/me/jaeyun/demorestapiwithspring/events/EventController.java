06ed3ee50c5ca0b28f5de44ba3eb86fbef6d4b17
JaeYoon Song <acornjello14@gmail.com>
2018-12-15 23:02:53 +0900

    rest-api : Apply Spring HATEOAS

--- Spring/demo-rest-api-with-spring/src/main/java/me/jaeyun/demorestapiwithspring/events/EventController.java
+++ Spring/demo-rest-api-with-spring/src/main/java/me/jaeyun/demorestapiwithspring/events/EventController.java
@@ -2,6 +2,7 @@ package me.jaeyun.demorestapiwithspring.events;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
@@ -53,8 +54,14 @@ public class EventController {
         Event event = modelMapper.map(eventDto, Event.class);
         event.update();
         Event newEvent = this.eventRepository.save(event);
+        ControllerLinkBuilder selfLinkBuilder = linkTo(EventController.class);
         URI createUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
 
-        return ResponseEntity.created(createUri).body(event);
+        EventResource eventResource = new EventResource(event);
+        eventResource.add(linkTo(EventController.class).withRel("query-events"));
+        //eventResource.add(selfLinkBuilder.withSelfRel());
+        eventResource.add(selfLinkBuilder.withRel("update-event"));
+
+        return ResponseEntity.created(createUri).body(eventResource);
     }
 }
---
+++ Spring/demo-rest-api-with-spring/src/main/java/me/jaeyun/demorestapiwithspring/events/EventResource.java
@@ -0,0 +1,26 @@
+package me.jaeyun.demorestapiwithspring.events;
+
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.ResourceSupport;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+
+public class EventResource extends Resource<Event> { // extends ResourceSupport {
+
+    public EventResource(Event event, Link... links) {
+        super(event, links);
+        add(linkTo(EventController.class).slash(event.getId()).withSelfRel());
+    }
+//    @JsonUnwrapped
+//    private Event event;
+//
+//    public EventResource(Event event) {
+//        this.event = event;
+//    }
+//
+//    public Event getEvent() {
+//        return event;
+//    }
+}
