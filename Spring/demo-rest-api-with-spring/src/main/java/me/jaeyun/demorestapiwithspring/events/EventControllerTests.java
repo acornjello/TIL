21f483a7e6aa460d31df19e6896fae89acde5fd0
JaeYoon Song <acornjello14@gmail.com>
2018-12-27 23:25:50 +0900

    rest-api : Build docs/index.html

--- Spring/demo-rest-api-with-spring/src/main/java/me/jaeyun/demorestapiwithspring/events/EventControllerTests.java
+++ Spring/demo-rest-api-with-spring/src/main/java/me/jaeyun/demorestapiwithspring/events/EventControllerTests.java
@@ -79,8 +79,8 @@ public class EventControllerTests {
                         links(
                                 linkWithRel("self").description("link to self"),
                                 linkWithRel("query-events").description("link to query events"),
-                                linkWithRel("update-event").description("link to update an existing events")
-
+                                linkWithRel("update-event").description("link to update an existing events"),
+                                linkWithRel("profile").description("link to profile")
                         ),
                         requestHeaders(
                                 headerWithName(HttpHeaders.ACCEPT).description("accept header"),
